454910fe6644ceccf6a1a729852d623c48098d26
Michael Tran <michaeltran.online@gmail.com>
2021-05-15 19:52:03 +1000

    AD-3 fix multiple runtime errors

--- src/helpers/notify.ts
+++ src/helpers/notify.ts
@@ -1,23 +1,25 @@
 import { HttpError, NotificationType } from 'react-admin';
 
-export const notifyOnFailure = (
-  notify: (
-    message: string,
-    type: NotificationType | undefined,
-    messageArgs: unknown,
-    undoable?: boolean,
-    autoHideDuration?: number | undefined,
-  ) => void,
-) => (error: HttpError): void => {
-  const { errors }: { errors: { title: string }[] } = error.body;
-  if (errors && errors.length)
-    notify(
-      `Please check the following ${errors
-        .map((e: { title: string }) => `* ${e.title},`)
-        .join('')}`,
-      'error',
-      {},
-      false,
-      20000, // 20 seconds
-    );
-};
+export const notifyOnFailure =
+  (
+    notify: (
+      message: string,
+      type: NotificationType | undefined,
+      messageArgs: unknown,
+      undoable?: boolean,
+      autoHideDuration?: number | undefined,
+    ) => void,
+  ) =>
+  (error: HttpError): void => {
+    const { errors }: { errors: { title: string }[] } = error.body;
+    if (errors && errors.length)
+      notify(
+        `Please check the following ${errors
+          .map((e: { title: string }) => `* ${e.title},`)
+          .join('')}`,
+        'error',
+        {},
+        false,
+        20000, // 20 seconds
+      );
+  };
