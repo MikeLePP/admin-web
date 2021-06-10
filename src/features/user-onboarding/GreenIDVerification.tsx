cae65ac120ba49664ae2696f515e9c3b20a9e3af
Ronak <ronakamani@gmail.com>
2021-06-10 10:17:31 +1000

    add greenId ui

--- src/features/user-onboarding/GreenIDVerification.tsx
+++ src/features/user-onboarding/GreenIDVerification.tsx
@@ -7,65 +7,23 @@ interface GreenIDVerificationValues {
   userDetails: User;
 }
 
-const isUserIdentityVerified = async (userDetails: User): Promise<boolean> => {
-  const { status, body } = await callApi(`/identities/`, 'put', {
-    data: {
-      attributes: { ...userDetails },
-    },
-  });
-  if (status === 200) {
-    const {
-      data: {
-        attributes: { verified },
-      },
-    } = JSON.parse(body);
-    return verified as boolean;
-  }
-  return false;
-};
-
-const isUserPassportsVerified = async (userDetails: User): Promise<boolean> => {
-  const { status, body } = await callApi(`/identities/${userDetails.identity.id}/passport`, 'put', {
-    data: {
-      attributes: { passportNumber: userDetails.identity.number },
-    },
-  });
-  if (status === 200) {
-    const {
-      data: {
-        attributes: { verified },
-      },
-    } = JSON.parse(body);
-    return verified as boolean;
-  }
-  return false;
-};
-
 const GreenIDVerification = ({ userDetails }: GreenIDVerificationValues): JSX.Element => {
   const [state, setState] = useState({
     isFirstRender: true,
     isGreenIDVerified: false,
   });
   const checkGreenID = async () => {
-    const identityVerified = await isUserIdentityVerified(userDetails);
-    if (identityVerified) {
+    const identityVerification = await callApi(`/users/${userDetails.id}/identities`, 'put');
+    if (identityVerification) {
       setState({
         isFirstRender: false,
         isGreenIDVerified: true,
       });
     } else {
-      const passportVerified = await isUserPassportsVerified(userDetails);
-      if (passportVerified) {
-        setState({
-          isFirstRender: false,
-          isGreenIDVerified: true,
-        });
-      } else {
-        setState({
-          isFirstRender: false,
-          isGreenIDVerified: false,
-        });
-      }
+      setState({
+        isFirstRender: false,
+        isGreenIDVerified: false,
+      });
     }
   };
   return (
