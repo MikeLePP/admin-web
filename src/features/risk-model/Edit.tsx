13678cfa142a00d70af31a1bb491d6fd25ab9f97
spotlight <thienanhnguyen06@gmail.com>
2021-07-20 14:01:57 +0700

    CORE-29 Update Edit UI

--- src/features/risk-model/Edit.tsx
+++ src/features/risk-model/Edit.tsx
@@ -7,7 +7,9 @@ import {
   Paper,
   TextField,
 } from '@material-ui/core';
-import { get } from 'lodash';
+import { SaveOutlined as SaveIcon, Add as CreateIcon } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import { get, set } from 'lodash';
 import {
   Edit,
   Record,
@@ -21,27 +23,74 @@ import {
   maxValue,
 } from 'react-admin';
 import { useFormik } from 'formik';
-
 import { RowsData, StyledTableCell, CellWithRightBorder } from './common';
-
 import EditToolbar from '../../components/EditToolbar';
-import SaveToolbar from '../../components/SaveToolbar';
+import { SaveButton, Toolbar } from 'react-admin';
+import { Button } from '@material-ui/core';
 import { notifyOnFailure } from '../../helpers/notify';
-import { getId } from '../../helpers/url';
 import { useRiskModel } from './risk-model-hooks';
-
+import { useState, useEffect } from 'react';
+import { RiskModel } from '../../types/risk-model';
+type SaveToolbarProps = {
+  saveButtonLabel?: string;
+  pristine?: boolean;
+  currentRiskModel?: RiskModel
+};
+const EditSaveToolbar = ({
+  saveButtonLabel = 'Save',
+  pristine,
+  currentRiskModel,
+  ...rest
+}: SaveToolbarProps): JSX.Element => {
+  const history = useHistory();
+  let icon;
+  switch (saveButtonLabel) {
+    case 'Save':
+      icon = <SaveIcon />;
+      break;
+    case 'Create':
+      icon = <CreateIcon />;
+      break;
+    default:
+  }
+  const handleCancelClick = () => {
+    history.goBack();
+  };
+  const handleSave = (): void => {
+    // Call PUT API
+  };
+  return (
+    <Toolbar {...rest}>
+      <SaveButton
+        onClick={handleSave}
+        label={saveButtonLabel}
+        icon={icon}
+        disabled={false}
+        className="mr-2"
+      />
+      <Button onClick={handleCancelClick}>Cancel</Button>
+    </Toolbar>
+  );
+};
 const RiskModelEdit = (props: ResourceComponentPropsWithId): JSX.Element | null => {
   const riskModelId = get(props, 'id', '');
   const { riskModel } = useRiskModel(riskModelId);
+  const [value, setValue] = useState<RiskModel | undefined>();
   const approvedLimits = riskModel?.ruleSets.map((ruleSet) =>
     ruleSet.approvedLimit ? ruleSet.approvedLimit : undefined,
   );
   const notify = useNotify();
-  const { record } = useEditController(props);
-
+  useEffect(() => {
+    if (riskModel) {
+      setValue(riskModel);
+    }
+  }, [riskModel]);
   const onRiskModelParameterChange =
-    (parameterPath: string) => (e: React.ChangeEvent<HTMLInputElement>) => {};
-
+    (parameterPath: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const currentValue = e.target.value;
+      console.log("---", parameterPath, currentValue)
+      // Update the "value" state with parameterPath and currentValue inside here
+    };
   return (
     <Edit
       {...props}
@@ -51,7 +100,7 @@ const RiskModelEdit = (props: ResourceComponentPropsWithId): JSX.Element | null 
       mutationMode="pessimistic"
       actions={<EditToolbar />}
     >
-      <SimpleForm>
+      <SimpleForm toolbar={<EditSaveToolbar pristine={false} currentRiskModel={value} />}>
         <TextInput source="name" />
         <TableContainer component={Paper} style={{ width: '100%' }}>
           <Table>
@@ -78,13 +127,10 @@ const RiskModelEdit = (props: ResourceComponentPropsWithId): JSX.Element | null 
                   {approvedLimits?.map((approvedLimit, index) => (
                     <CellWithRightBorder align="right">
                       {row.parameterPath && (
-                        <TextField
+                        <TextField 
                           id="filled-number"
                           type="number"
-                          InputLabelProps={{
-                            shrink: true,
-                          }}
-                          value={
+                          defaultValue={
                             row.parameterPath &&
                             get(riskModel, `ruleSets[${index}].${row.parameterPath}`)
                           }
@@ -104,5 +150,4 @@ const RiskModelEdit = (props: ResourceComponentPropsWithId): JSX.Element | null 
     </Edit>
   );
 };
-
 export default RiskModelEdit;
