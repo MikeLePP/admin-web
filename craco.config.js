0375f6ee610a682b2c854bee740d528e246940a9
Tri Q Tran <2232035+triqi@users.noreply.github.com>
2021-08-08 21:34:54 +1000

    Fix CI build error

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,6 @@
 /* eslint-disable global-require */
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   eslint: {
     enable: false,
@@ -7,5 +9,22 @@ module.exports = {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
+  },
+  webpack: {
+    configure: (webpackConfig) => ({
+      ...webpackConfig,
+      optimization: {
+        ...webpackConfig.optimization,
+        // Workaround for CircleCI bug caused by the number of CPUs shown
+        // https://github.com/facebook/create-react-app/issues/8320
+        minimizer: webpackConfig.optimization.minimizer.map((item) => {
+          if (item instanceof TerserPlugin) {
+            item.options.parallel = 0;
+          }
+
+          return item;
+        }),
+      },
+    }),
   },
 };
